beb94f4e69a923b39f0aa9dfe4893e8bfb5e3483
Samuli Yliniemi <15356838+samuliy@users.noreply.github.com>
2021-09-01 10:17:09 +0300

    Create ClassicUO settings.json if it doesn't exist

--- ClassicUO/docker-entrypoint.run.sh
+++ ClassicUO/docker-entrypoint.run.sh
@@ -4,6 +4,44 @@ set -e
 
 tmpfile=$(mktemp)
 
+if [[ ! -f /ClassicUO/bin/Debug/settings.json ]]; then
+	cat << SETTINGS > /ClassicUO/bin/Debug/settings.json
+{
+	"username" : "",
+	"password" : "",
+	"ip" : "",
+	"port" : "",
+	"ultimaonlinedirectory" : "",
+	"profilespath" : "",
+	"clientversion" : "",
+	"lastcharactername" : "",
+	"lang" : "",
+	"lastservernum" : 1,
+	"last_server_name" : "",
+	"fps" : 60,
+	"window_position" : null,
+	"window_size" : null,
+	"is_win_maximized" : true,
+	"saveaccount" : false,
+	"autologin" : false,
+	"reconnect" : false,
+	"reconnect_time" : 1,
+	"login_music" : true,
+	"login_music_volume" : 70,
+	"shard_type" : 0,
+	"fixed_time_step" : true,
+	"run_mouse_in_separate_thread" : true,
+	"force_driver" : 0,
+	"use_verdata" : false,
+	"maps_layouts" : null,
+	"encryption" : 0,
+	"plugins" : [
+		"./Assistant/Razor.dll"
+	]
+}
+SETTINGS
+fi
+
 jq ".ip = \"$UO_SERVER_HOST\" | .port = \"$UO_SERVER_PORT\" | .ultimaonlinedirectory = \"../UOData\"" < /ClassicUO/bin/Debug/settings.json > "$tmpfile"
 cd /ClassicUO/bin/Debug
 mv -f $tmpfile settings.json
